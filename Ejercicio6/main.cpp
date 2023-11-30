97ea376501ec0ccf08738fd4c1750cf3fa4d038d
ddealsan <ddealsan@myuax.com>
2023-11-30 16:03:04 +0100

    Completamos el codigo

--- Ejercicio6/main.cpp
+++ Ejercicio6/main.cpp
@@ -3,3 +3,18 @@
 
 using namespace std;
 using namespace boost;
+
+int main() {
+    string s;
+    cout << "Ingresa una cadena de caracteres: ";
+    cin >> s;
+
+    try {
+        int i = lexical_cast<int>(s);
+        cout << "Número: " << i << endl;
+    } catch(bad_lexical_cast &e) {
+        cerr << "Error: " << e.what() << endl;
+    }
+
+    return 0;
+}
