51eee78ee859fb60b9ebc78bd85754b4bfc03c40
ddealsan <ddealsan@myuax.com>
2023-11-30 11:26:42 +0100

    Añadimos el main

--- Ejercicio3/main.cpp
+++ Ejercicio3/main.cpp
@@ -3,3 +3,10 @@
 #include <algorithm>
 
 using namespace std;
+
+
+int main() {
+
+    vector<int> miVector = {50, 20, 60, 10, 30, 40};
+    sort(miVector.begin(), miVector.end());
+    cout << "Elementos del vector ordenados:" << std::endl;
