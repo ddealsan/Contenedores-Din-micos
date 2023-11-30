a676c84f2bb50ae8f3ecd4a2eb2967478d5388e1
ddealsan <ddealsan@myuax.com>
2023-11-30 11:19:01 +0100

    Añadimos parte del codigo

--- Ejercicio2/main.cpp
+++ Ejercicio2/main.cpp
@@ -4,3 +4,10 @@
 
 using namespace std;
 
+int main(){
+    list<float> miLista={1.1,2.2,3.3,4.4,5.5};
+    stack<float, list<float>> miPila(miLista);
+    cout << "Elementos de la pila: " << endl;
+
+
+}
