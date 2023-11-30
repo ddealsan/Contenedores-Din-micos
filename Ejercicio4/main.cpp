e4d659868344365b4702b85fc0f4204171bdcd13
ddealsan <ddealsan@myuax.com>
2023-11-30 11:34:05 +0100

    definimos los valarrays

--- Ejercicio4/main.cpp
+++ Ejercicio4/main.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 #include <valarray>
+
+using namespace std;
+int main(){
+    valarray<double> valores = {1.0, 2.0, 3.0, 4.0, 5.0};
+    valarray<double> suma = valores + 10.0;
+    valarray<double> resta = valores - 2.0;
+    valarray<double> multiplicacion = valores * 3.0;
+    valarray<double> division = valores / 2.0;
+}
